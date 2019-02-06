6045fb79e53a17c61d2b8e3d98d4eb9a335c5fda
berkturan <berk.turan2@isik.edu.tr>
2019-02-06 15:51:48 +0300

    Initial Commit

---
+++ TurkishKit-UISwitch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TurkishKit-UISwitch
+//
+//  Created by Berk Turan on 6.02.2019.
+//  Copyright © 2019 Berk Turan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
